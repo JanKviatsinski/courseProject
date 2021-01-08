1cc995d27d4118d04ea35b80052b2bca6f3a4a35
JanKviatsinski <yan5751839@gmail.com>
2021-01-08 22:03:49 +0300

    header

--- src/js/JS.js
+++ src/js/JS.js
@@ -1,1 +1,13 @@
-console.log(42241)
+'use strict'
+const headerInformationOwner = document.body.querySelector('.header__information-owner');
+headerInformationOwner.hidden = true;
+
+const btnInformationOwner = document.body.querySelector('.header__btn-information-owner');
+btnInformationOwner.addEventListener('click', () =>{
+     headerInformationOwner.hidden = !headerInformationOwner.hidden;
+      (!headerInformationOwner.hidden) ?
+          btnInformationOwner.style.animationName = 'none' :
+          btnInformationOwner.style.animationName = 'button-animation';
+});
+
+
