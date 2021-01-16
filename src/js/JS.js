1a0924dfc58a638dbab3a0c3b23d195b4c40b69b
JanKviatsinski <yan5751839@gmail.com>
2021-01-16 23:06:51 +0300

    87

--- src/js/JS.js
+++ src/js/JS.js
@@ -10,4 +10,26 @@ btnInformationOwner.addEventListener('click', () =>{
           btnInformationOwner.style.animationName = 'button-animation';
 });
 
+// function debounce(cb, ms) {
+//     let timeoutID = null;
+//
+//     return value => {
+//         if (timeoutID) {
+//             return;
+//         }
+//
+//         timeoutID = setTimeout(() => {
+//             cb(value);
+//             timeoutID = null;
+//         }, ms);
+//     };
+// }
+//
+// const f = debounce(value => console.log(`Hey! ${value}`), 1000);
+//
+// f(1);
+// setTimeout(() => f(2), 500);
+// setTimeout(() => f(3), 1500);
+// setTimeout(() => f(4), 2000);
+
 
