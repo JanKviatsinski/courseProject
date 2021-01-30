862989ef991effe5cd8ea03a261028861f2680de
JanKviatsinski <yan5751839@gmail.com>
2021-01-30 22:13:19 +0300

    92 database

--- src/js/main-header.js
+++ src/js/main-header.js
@@ -2,7 +2,7 @@
 const greetingOwner = document.body.querySelector('.greeting-owner-wrap');
 greetingOwner.hidden = true;
 
-const btnGreetingOwner = document.body.querySelector('.main-header__btn-greeting-owner');
+const btnGreetingOwner = document.body.querySelector('.main-header__btn--greeting');
 btnGreetingOwner.addEventListener('click', () =>{
      greetingOwner.hidden = !greetingOwner.hidden;
     btnGreetingOwner.style.animationName = !greetingOwner.hidden ? 'none' : 'button-animation';
