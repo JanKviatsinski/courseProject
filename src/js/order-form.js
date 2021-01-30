f4610e8231ad1206328471f28d4c7de396312ae5
JanKviatsinski <yan5751839@gmail.com>
2021-01-30 12:36:49 +0300

    90 database

--- src/js/order-form.js
+++ src/js/order-form.js
@@ -2,7 +2,8 @@ const orderForm = document.querySelector('#order-form');
 const url = "https://course-project-kviatsinski-default-rtdb.firebaseio.com/orders.json"
 const modalOrderForm = document.querySelector('.order-form__modal');
 const modalOrderFormWrap = modalOrderForm.querySelector('.order-form__wrap-modal');
-
+const paragraphModalOrder = modalOrderFormWrap.querySelector('.order-form__modal-paragraph')
+const table = document.createElement('table');
 orderForm.addEventListener('submit', async (e) => {
         e.preventDefault();
 
@@ -22,10 +23,10 @@ orderForm.addEventListener('submit', async (e) => {
                 productIsSelected = true;
             }
         }
+
         if (!productIsSelected) {
             modalOrderForm.style.display = 'block';
-            modalOrderFormWrap.textContent = 'Вы не выбрали ни одного продукта';
-            console.log('вы не выбрали ни одного продукта')
+            paragraphModalOrder.textContent = 'Вы не выбрали ни одного продукта';
             return false;
         }
 
@@ -35,14 +36,16 @@ orderForm.addEventListener('submit', async (e) => {
             const response = await fetch(url);
             orders = await response.json();
             let modifiedObjOrders = {};
+
             for (let orderId in orders) {
-                modifiedObjOrders [orders[orderId].name] = {...orders[orderId]};
-                modifiedObjOrders [orders[orderId].name].id = orderId;
+                modifiedObjOrders[orders[orderId][userName.placeholder]] = {...orders[orderId]};
+                modifiedObjOrders[orders[orderId][userName.placeholder]].id = orderId;
             }
+
             orders = modifiedObjOrders;
         } catch (error) {
             modalOrderForm.style.display = 'block';
-            modalOrderFormWrap.textContent =
+            paragraphModalOrder.textContent =
                 'Упс, что-то пошло не так. Попробуйте пожалуйста позже.';
             console.log('ОШИБКА', error)
             return new Error(error)
@@ -51,28 +54,23 @@ orderForm.addEventListener('submit', async (e) => {
         let nameInDatabase = false;
 
         if (Object.keys(orders).length === 0) {
-            console.log('нет базы')
             postOrder(order, url)
         } else if (orders[userName.value]) {
             modalOrderForm.style.display = 'block';
-            modalOrderFormWrap.textContent =
+            paragraphModalOrder.textContent =
                 'Заказ с таким именем уже есть. Используйте пожалуста другое имя.';
-            console.log('имя есть в базе');
             nameInDatabase = true;
-        } else {
-            console.log('имя НЕТ в базе')
         }
 
         if (!nameInDatabase) {
-            console.log('сделаем новый')
             postOrder(order, url);
         }
 
         async function postOrder(data, url) {
-            order[userName.placeholder]= userName.value;
+            order[userName.placeholder] = userName.value;
             order[userNote.placeholder] = userNote.value;
             try {
-                const response = await fetch(
+                /*const response = await*/ fetch(
                     url, {
                         method: 'Post',
                         headers: {
@@ -80,14 +78,14 @@ orderForm.addEventListener('submit', async (e) => {
                         },
                         body: JSON.stringify(data),
                     });
-                const result = await response.json();
+                // const result = await response.json();
                 modalOrderForm.style.display = 'block';
-                modalOrderFormWrap.textContent =
-                    `Отлично! Заказ принят. Ваш заказ ${result}` ;
-                console.log('новый заказ зарегестрирован', data);
+                paragraphModalOrder.textContent = 'Отлично! Заказ принят.';
+
+                createTableOrder(modalOrderFormWrap, data);
             } catch (error) {
                 modalOrderForm.style.display = 'block';
-                modalOrderFormWrap.textContent =
+                paragraphModalOrder.textContent =
                     'Упс, что-то пошло не так. Попробуйте пожалуйста позже.';
                 console.log('ОШИБКА', error)
             }
@@ -98,10 +96,10 @@ orderForm.addEventListener('submit', async (e) => {
 function delet() {
     fetch(url, {method: 'DELETE',})
 }
-
 // delet ()
 document.body.addEventListener('click', (event) =>{
     const clickObj = event.target;
+
     if (clickObj !== modalOrderForm){
         modalOrderForm.style.display = 'none';
     }
@@ -113,88 +111,18 @@ document.body.addEventListener('click', (event) =>{
     //     btnGreetingOwner.style.animationName = 'button-animation';
 })
 
+function createTableOrder(location, data){
+    for (let listPosition in data) {
+            const tableRow = table.insertRow();
+            const point = tableRow.insertCell();
+            const value = tableRow.insertCell();
+        point.textContent = listPosition;
+        value.textContent = data[listPosition];
+    }
 
+    location.append(table);
 
+    const tablePreviousSibling = table.previousElementSibling;
+    tablePreviousSibling.style.marginBottom = 1 + 'rem';
+}
 
-
-
-
-
-
-// const orderForm = document.querySelector('#order-form');
-// const url = "https://course-project-kviatsinski-default-rtdb.firebaseio.com/orders.json"
-//
-// orderForm.addEventListener('submit', async (e) => {
-//         e.preventDefault();
-//
-//         const userNote =
-//             orderForm.querySelector('#order-form__textarea').value;
-//         const userName =
-//             document.getElementsByName('user-name')[0].value;
-//         const inputs = document.getElementsByTagName('input');
-//         const order = {};
-//
-//         let productIsSelected = false;
-//
-//         for (let input of inputs) {
-//             if (input.checked) {
-//                 order[input.name] = input.value;
-//                 productIsSelected = true;
-//             }
-//         }
-//
-//         if (!productIsSelected) {
-//             console.log('вы не выбрали ни одного продукта')
-//             return false;
-//         }
-//
-//         let orders;
-//
-//         try {
-//             const response = await fetch(url);
-//             orders = await response.json();
-//         } catch (error) {
-//             console.log('ОШИБКА', error)
-//         }
-//
-//         let nameInDatabase = false;
-//
-//         if (orders === null) {
-//             console.log('нет базы')
-//             postOrder(order, url)
-//         } else {
-//             for (let order in orders) {
-//                 if (orders[order].name === userName) {
-//                     console.log('имя есть в базе');
-//                     nameInDatabase = true;
-//                 } else {
-//                     console.log('имя НЕТ в базе')
-//                 }
-//             }
-//         }
-//
-//         if (!nameInDatabase) {
-//             console.log('сделаем новый')
-//             postOrder(order, url)
-//         }
-//
-//         async function postOrder(data, url) {
-//             order.name = userName;
-//             order.note = userNote;
-//             try {
-//                 const response = await fetch(
-//                     url, {
-//                         method: 'Post',
-//                         headers: {
-//                             'Content-Type': 'application/json;charset=utf-8'
-//                         },
-//                         body: JSON.stringify(data),
-//                     });
-//                 const result = await response.json();
-//                 console.log('новый заказ зарегестрирован', result);
-//             } catch (error) {
-//                 console.log('ОШИБКА', error)
-//             }
-//         }
-//     }
-// )
